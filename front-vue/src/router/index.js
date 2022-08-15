4219e90f8f88555d361607757d8303733ecd35c8
Hans Kinkelaar <hanskinkelaar92@gmail.com>
2022-08-15 16:39:10 -0300

    Estruturando Projeto.

--- front-vue/src/router/index.js
+++ front-vue/src/router/index.js
@@ -4,7 +4,11 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../components/pages/list-pokemons/ListPokemonsComponent.vue'),
+  },
 ];
 
 const router = new VueRouter({

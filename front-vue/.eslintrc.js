e0cfc70ddcd93b2258c2cf683f77dd6fd922db08
Hans Kinkelaar <hanskinkelaar92@gmail.com>
2022-08-15 16:38:16 -0300

    Adicionando regras ao ESLINT

--- front-vue/.eslintrc.js
+++ front-vue/.eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     parser: '@babel/eslint-parser',
   },
   rules: {
+    'no-trailing-spaces': 'off',
+    'vuejs-accessibility/anchor-has-content': 'off',
+    'max-len': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },

60662469b7583462161db489d35ebfbc12b2b43f
Hans Kinkelaar <hanskinkelaar92@gmail.com>
2022-08-16 17:50:59 -0300

    arrumando o router porque tava me agoniando.

--- src/router/index.js
+++ src/router/index.js
@@ -14,9 +14,10 @@ const routes = [
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+export default router;

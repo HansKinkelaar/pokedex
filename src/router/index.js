2c3e384040ce3296db99b4a65253a9fe5efa2b42
Hans Kinkelaar <hanskinkelaar92@gmail.com>
2022-08-18 11:38:49 -0300

    Arrumando o router pra funcionar certinho.

--- src/router/index.js
+++ src/router/index.js
@@ -14,10 +14,9 @@ const routes = [
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-});
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
-export default router;
+export default router
